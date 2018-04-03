0e07993805dbfa63d9ab7b85d53e99d90e534f50
zdcthomas <zdcthomas@yahoo.com>
2018-04-02 23:35:12 -0600

    successfully tests seperated response class

--- lib/response.rb
+++ lib/response.rb
@@ -1,21 +1,22 @@
 class Response
-  def initialize(output)
-    @output = output
-  end
-
-  def respond_with_header
-    @output = "<html><head></head><body>#{@output}</body></html>"
-    @headers = ["http/1.1 200 ok",
+  attr_reader :body,
+              :header,
+              :output
+  def initialize(body)
+    @body = body
+    @output = "<html><head></head><body>#{body}</body></html>"
+    @header = ["http/1.1 200 ok",
               "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
               "server: ruby",
               "content-type: text/html; charset=iso-8859-1",
-              "content-length: #{output.length}\r\n\r\n"].join("\r\n")
-    @client.puts headers
-    @client.puts output
-    puts "Header: \n\r #{headers}\r\nResponse: #{@output}"
+              "content-length: #{@output.length}\r\n\r\n"].join("\r\n")
   end
 
-  def
+  def respond_with_header
+    [@header, @output]
+  end
 
+  def report_response
+    "Header: \n\r #{@header}\r\nResponse: #{@output}"
   end
 end
---
+++ test/response_test.rb
@@ -0,0 +1,34 @@
+require "Minitest/autorun"
+require "Minitest/pride"
+require_relative "../lib/response"
+
+class ResponseTest < MiniTest::Test
+  def setup
+  end
+
+  def test_it_exists
+    response = Response.new("Hello World")
+    assert_instance_of Response, response
+  end
+
+  def test_response_has_header
+    response = Response.new("Hello World")
+    expected = "http/1.1 200 ok"
+    actual = response.header.split("\r\n")[0]
+    assert_equal expected, actual
+  end
+
+  def test_it_responds_with_header_and_body
+    response = Response.new("Hello World")
+    assert_equal [response.header, response.output], response.respond_with_header
+  end
+
+  def test_it_reports_response
+    response = Response.new("Hello World")
+    expected = "Header: \n\r #{response.header}\r\nResponse: #{response.output}"
+    actual = response.report_response
+    assert_equal expected, actual
+  end
+
+
+end
