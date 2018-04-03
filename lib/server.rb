ae9e4f60bdd98e39b5aebcc365a90c6669022783
zdcthomas <zdcthomas@yahoo.com>
2018-04-02 21:36:57 -0600

    TESTS units of server class, will break apart server class in future commit WIP

--- lib/server.rb
+++ lib/server.rb
@@ -26,8 +26,8 @@ class Serv
     puts "Connection Terminated"
   end
 
-  def response(string)
-    output = "<html><head></head><body>#{string}</body></html>"
+  def response(output)
+    output = "<html><head></head><body>#{output}</body></html>"
     headers = ["http/1.1 200 ok",
               "date: #{Time.now.strftime('%a, %e %b %Y %H:%M:%S %z')}",
               "server: ruby",
@@ -35,7 +35,7 @@ class Serv
               "content-length: #{output.length}\r\n\r\n"].join("\r\n")
     @client.puts headers
     @client.puts output
-    puts "Header: \n\r #{headers}\r\nResponse: #{string}"
+    puts "Header: \n\r #{headers}\r\nResponse: #{output}"
   end
 
   def diagnostics
@@ -48,4 +48,10 @@ class Serv
     Accepts: #{@parser["Accepts"]}\n"
   end
 
+end
+while server.begin_connection
+  server.receive_request_lines
+  server.response(server.request_lines)
+  server.diagnostics
+  server.terminate
 end
--- test/server_test.rb
+++ test/server_test.rb
@@ -9,21 +9,25 @@ require "Minitest/pride"
 class ServerTest < MiniTest::Test
   def setup
   end
+  def test_it_exists
+    server = Serv.new(9292)
+    assert_instance_of Serv, server
+  end
+
+  def test_begin_connection
+    server = Serv.new(9292)
+
+  end
+
+  def test_it_can_receive_request_lines
+  end
+
+  def test_it_can_terminate
+  end
 
   def test_server_can_connect
     response = Faraday.get('http://localhost:9292/')
     assert_equal 200, response.status
   end
 
-  def test_server_can_receive_request
-    skip
-  end
-
-  def test_server_can_return_hello_world
-    skip
-  end
-
-  def test_server_can_track_previous_request
-    skip
-  end
 end
