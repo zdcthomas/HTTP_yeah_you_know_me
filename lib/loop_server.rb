2b9221a98eee60409ebfea323873c51097afc10a
zdcthomas <zdcthomas@yahoo.com>
2018-04-04 11:28:45 -0600

    Implements server loop, still am unsure how to test units on server based classes

---
+++ lib/loop_server.rb
@@ -0,0 +1,32 @@
+require './lib/server'
+require './lib/parser'
+require './lib/response'
+require './lib/conductor'
+require 'pry'
+
+class ServerLoop
+  def initialize(port)
+    @server = Serv.new(port)
+    @conductor = Conductor.new
+  end
+
+  def start_loop
+    loop do
+      @server.begin_connection
+      # binding.pry
+      @server.receive_request_lines
+      request = @server.request_lines
+      page = @conductor.conduct(request)
+      # binding.pry
+      @response = Response.new(page)
+      puts @response.report_response
+      @server.client.puts @response.respond_with_header.join
+      # binding.pry
+      if page[1] == "exit"
+        exit
+      end
+      @server.terminate
+    end
+  end
+
+end
---
+++ lib/runner.rb
@@ -0,0 +1,3 @@
+require './lib/loop_server'
+loop = ServerLoop.new(9292)
+loop.start_loop
